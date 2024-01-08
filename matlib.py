be0fa680fb3d442cbc01b060b1de747908defd58
csheanty <csheanttel@gmail.com>
2024-01-08 20:29:10 +0800

    Madlibs activity

---
+++ matlib.py
@@ -0,0 +1,8 @@
+#MADLIBS
+
+person = input("Person in the room ")
+age = input("Age ")
+adj1 = input("Adjective ")
+color = input("Color ")
+
+print(f"My name is {person} and I am {age} years old. If I were president, I'd do a whole bunch of {adj1} things: \n 1. I would drive the biggest {color} car in the country.")
